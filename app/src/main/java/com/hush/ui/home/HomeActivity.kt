60c3172ecd3b3578ad7a175590296983919bf566
tdevika <devika.thigaram@gmail.com>
2019-11-24 23:00:52 +0100

    clean

--- app/src/main/java/com/hush/ui/home/HomeActivity.kt
+++ app/src/main/java/com/hush/ui/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.hush.ui.home
 
 import android.os.Bundle
 import androidx.navigation.Navigation.findNavController
+import com.google.android.material.tabs.TabLayout
 import com.hush.R
 import com.hush.databinding.ActivityHomeBinding
 import com.hush.ui.base.BaseActivity
@@ -19,5 +20,22 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         ThemeOverlayUtils.applyThemeOverlays(this)
         super.onCreate(savedInstanceState)
+        var pageAdapter = PageAdapter(supportFragmentManager, binding.tabLayout.getTabCount())
+        binding.pager.setAdapter(pageAdapter)
+       binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                binding.pager.setCurrentItem(tab.position)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+        binding.pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
+
     }
 }
